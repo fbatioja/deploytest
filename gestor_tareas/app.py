92fb7c9ba83b1de1a8da4f74de1b7c98796951c4
Fabian Batioja <f.batioja@uniandes.edu.co>
2021-12-01 20:29:51 -0500

    agregar RDBMS variable de entorno

--- gestor_tareas/app.py
+++ gestor_tareas/app.py
@@ -11,12 +11,14 @@ dbHost = os.environ.get("DB_HOST")
 dbNameGestorTareas = os.environ.get("DB_NAME_GESTORTAREAS")
 dbUser = os.environ.get("DB_USER")
 dbPassword = os.environ.get("DB_PASSWORD")
+rdbms = os.environ.get("RDBMS", "mysql+pymysql")
+
 
 environment = os.environ.get("PROJECT_ENVIRONMENT")
 if environment == "develop":
     app.config['SQL_ALCHEMY_DATABASE_URI'] = 'sqlite:///gestor-tareas.db'
 else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{dbUser}:{dbPassword}@{dbHost}/{dbNameGestorTareas}"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"{rdbms}://{dbUser}:{dbPassword}@{dbHost}/{dbNameGestorTareas}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = os.environ["JWT_SECRET_KEY"]
 app.config['PROPAGATE_EXCEPTIONS'] = True
