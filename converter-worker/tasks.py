ff1dd9ea22b2f8a022169652b09727ed9264ef8a
fbatioja <f.batioja@uniandes.edu.co>
2021-12-04 17:01:35 -0500

    Update tasks.py

--- converter-worker/tasks.py
+++ converter-worker/tasks.py
@@ -31,7 +31,7 @@ def convert_task(filename, newFormat, userId, taskId, timecreated):
     logger.info(f'Solicitud de conversión - {filename} a {newFormat}')
     resp = convert_validation(filename, newFormat, userId)
     timeend = round(time.time())
-    #requests.post(f"{log_host}/logTransaction", json={"taskId": taskId,"timecreated": timecreated,"timestart": timestart,"diff": diff,"timeend": timeend})
+    requests.post(f"{log_host}/logTransaction", json={"taskId": taskId,"timecreated": timecreated,"timestart": timestart,"diff": diff,"timeend": timeend})
     if resp:
         logger.info(f"Conversión de archvio {filename} a {newFormat}")
         requests.post(f"{gestor_tareas_host}/updateTask",
